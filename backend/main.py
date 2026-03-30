a996eee166e4d44949ec844aff077c379a923af0
mawddah <163349829+mawddah@users.noreply.github.com>
2026-03-30 14:55:38 +0300

    fix: adjust python sys.path to resolve ModuleNotFoundError on vercel

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from fastapi import FastAPI, Depends, UploadFile, File, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
